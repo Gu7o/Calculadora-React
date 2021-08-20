976590494568876acc8b747431082029c260e087
Gu7o <gustavovv3@gmail.com>
2021-08-20 15:01:26 -0300

    Calculadora finalizada, podendo apenas melhorar deixando o display ser mais flexivel para grandes numeros

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,35 +11,35 @@ var resultado;
 var conta;
 var numeroArray = new Array();
 
-
 const Calculadora = () => {
     
-    const [numero, setNumero] = useState(9);
-
-
-
+    const [numero, setNumero] = useState('Display');
 
 
 function mostrarNumero(){
 /*Funcao mostraNumero que mostra no display o numero do operador 1 , operador 2 ou o resultado. Importante que 
 a parte condicional fique dentro do setNumero pois hooks nao podem estar dentro de loops,
 condicionais ou nested functions*/
+    console.log(estado);
     setNumero(()=>{
-        if (estado= 'operando1'){
+        if (estado == 'operando1'){
             op1=parseFloat(numeroArray.join(''));
-            //setNumero(op1); parece que hooks nao podem ser chamados dentro de condicionais
-            return op1;
             console.log(op1);
+            return op1;
+            
         }
-        if (estado= 'operando2'){
+        if (estado=='operando2'){
+            op2=parseFloat(numeroArray.join(''));
+            console.log('passei operando 2');
             return op2;
-            console.log(op2);
+            
         }
-        if (estado= 'resultado'){
-           return resultado;
-           console.log(resultado);
+        if (estado== 'resultado'){
+            console.log('o resultado e'+ resultado);
+            return resultado.toFixed(5);
+           
         }
-        if (estado ='Display'){
+        if (estado =='Display'){
             return 'Display'
         }
 
@@ -48,9 +48,8 @@ condicionais ou nested functions*/
 }
 function proximoNumero (digito){
     //Funcao que adiciona no array o proximo numero e vai montando o operando 1 ou 2
+    console.log(estado);
     numeroArray.push(digito);
-    console.log('passei proximo numero');
-    console.log(estado)
     mostrarNumero();
 
 }
@@ -60,30 +59,67 @@ function clear (){
     setNumero(0);
     numeroArray = [];
     estado = 'operando1';
+    console.log('Clear')
 }
 
 function operacao (sinal){
     /* Funcao Operacao : ela recebe dos botoes o sinal e dependendo do sinal ela vai mudar o 
 valor da variavel conta,futamente usado pra determinar a operacao, e mudar o estado para 
 'operando2' a fim do proximo numero digitado ser armazenado no segundo operador*/
+    estado = 'operando2';
+        
     switch (sinal){
         case 'soma':
-            estado = 'operando2';
-            conta = sinal; 
+            numeroArray = [];
+            conta = sinal;
+               
             break;
         case 'subtracao':
-            estado = 'operando2';
+            //estado = 'operando2';
+            numeroArray = [];
             conta = sinal; 
             break;
 
         case 'multiplicacao':
-            estado = 'operando2';
+            //estado = 'operando2';
+            numeroArray = [];
             conta = sinal; 
             break;
         case 'divisao':
-            estado = 'operando2';
+            //estado = 'operando2';
+            numeroArray = [];
             conta = sinal; 
             break;          
+    }
+}
+function deletar (){
+    numeroArray.pop();
+    mostrarNumero();
+}
+
+function igual (){
+    estado= 'resultado';
+    switch (conta){
+        case 'soma':
+            resultado= op1+op2;
+            console.log(op1);
+            console.log(op2);
+            console.log(resultado);
+            console.log('fiz a soma');
+            mostrarNumero();
+            break;
+        case 'subtracao':
+            resultado= op1-op2;
+            mostrarNumero();
+            break;
+        case 'multiplicacao':
+            resultado= op1*op2;
+            mostrarNumero();
+            break;
+        case 'divisao':
+            resultado= op1/op2;
+            mostrarNumero();
+            break;       
     }
 }
 
@@ -93,23 +129,23 @@ valor da variavel conta,futamente usado pra determinar a operacao, e mudar o est
         <div className='container'>
             <div className='display'>{numero}</div>
             <Botao estilo='botao2' onClick={() => {clear()}} text='AC'/>
-            <Botao estilo='botao' text='DEL'/>
-            <Botao estilo='botao' text='/'/>
+            <Botao estilo='botao' onClick={() => {deletar()}} text='DEL'/>
+            <Botao estilo='botao' onClick={() => {operacao('divisao')}} text='/'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(1)}} text='1'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(2)}} text='2'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(3)}} text='3'/>
-            <Botao estilo='botao' text='*'/>
+            <Botao estilo='botao' onClick={() => {operacao('multiplicacao')}}text='*'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(4)}} text='4'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(5)}} text='5'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(6)}} text='6'/>
-            <Botao estilo='botao' text='+'/>
+            <Botao estilo='botao' onClick={() => {operacao('soma')}} text='+'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(7)}} text='7'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(8)}} text='8'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(9)}} text='9'/>
-            <Botao estilo='botao' text='-'/>
-            <Botao estilo='botao' text='.'/>
+            <Botao estilo='botao' onClick={() => {operacao('subtracao')}}text='-'/>
+            <Botao estilo='botao' onClick= {()=>{proximoNumero('.')}}text='.'/>
             <Botao estilo='botao' onClick= {()=>{proximoNumero(0)}} text='0'/>
-            <Botao estilo='botao2' text='='/>
+            <Botao estilo='botao2'onClick= {()=>{igual()}} text='='/>
             
         </div>
     );
